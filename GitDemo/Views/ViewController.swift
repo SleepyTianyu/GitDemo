2c66eccdcccf46588f6660a7658e023be4b5e4f7
Tianyu <Tianyu041006@outlook.com>
2024-12-22 20:20:18 +0800

    config tabbar background with default appearance.

--- GitDemo/Views/ViewController.swift
+++ GitDemo/Views/ViewController.swift
@@ -14,18 +14,32 @@ class ViewController: UITabBarController {
         // Do any additional setup after loading the view.
         // Setup tab bar view controllers
         let firstVC = FirstViewController()
-        firstVC.view.backgroundColor = .red
+        firstVC.view.backgroundColor = .systemBackground
         firstVC.tabBarItem = UITabBarItem(tabBarSystemItem: .favorites, tag: 0)
         
         let secondVC = SecondViewController()
-        secondVC.view.backgroundColor = .green
+        secondVC.view.backgroundColor = .systemBackground
         secondVC.tabBarItem = UITabBarItem(tabBarSystemItem: .contacts, tag: 1)
         
         let thirdVC = ThirdViewController()
-        thirdVC.view.backgroundColor = .blue
-        thirdVC.tabBarItem = UITabBarItem(tabBarSystemItem: .downloads, tag: 2)
+        thirdVC.view.backgroundColor = .systemBackground
+        thirdVC.tabBarItem = UITabBarItem(tabBarSystemItem: .bookmarks, tag: 2)
 
-        setViewControllers([firstVC, secondVC, thirdVC], animated: false)
+        setViewControllers([firstVC, secondVC, thirdVC], animated: true)
+        
+        // 确保 tabBar 使用系统默认样式
+        if #available(iOS 13.0, *) {
+            let appearance = UITabBarAppearance()
+            appearance.configureWithDefaultBackground() // 恢复系统默认背景
+            tabBar.standardAppearance = appearance
+            
+            if #available(iOS 15.0, *) {
+                tabBar.scrollEdgeAppearance = appearance // 适配滚动边界样式
+            }
+        } else {
+            tabBar.isTranslucent = true // 确保半透明效果
+            tabBar.backgroundImage = nil
+            tabBar.shadowImage = nil
+        }
     }
-    
 }
